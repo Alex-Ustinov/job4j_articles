b267a5eda99458d5e7d0deb7b77384a8f939eef1
18485888 <AAlUstinov@sberbank.ru>
2021-08-13 01:51:22 +0300

    Тестовое задание GB Referense

--- src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
+++ src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
@@ -3,6 +3,7 @@ package ru.job4j.articles.service.generator;
 import ru.job4j.articles.model.Article;
 import ru.job4j.articles.model.Word;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,10 +12,14 @@ import java.util.stream.Collectors;
 public class RandomArticleGenerator implements ArticleGenerator {
     @Override
     public Article generate(List<Word> words) {
-        var wordsCopy = new ArrayList<>(words);
+        var wordsCopy = new ArrayList<SoftReference<Word>>();
+        for (int i = 0; i < words.size(); i++) {
+            wordsCopy.add(new SoftReference<Word>(words.get(i)));
+            words.remove(i);
+        }
         Collections.shuffle(wordsCopy);
         var content = wordsCopy.stream()
-                .map(Word::getValue)
+                .map(item -> item.get().getValue())
                 .collect(Collectors.joining(" "));
         return new Article(content);
     }
