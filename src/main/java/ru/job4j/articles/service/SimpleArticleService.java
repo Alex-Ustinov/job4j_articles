5180e0a9862ce2a3263062284373725c16e6a87e
18485888 <AAlUstinov@sberbank.ru>
2021-08-22 16:18:05 +0300

    Тестовое задание reference

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -7,16 +7,6 @@ import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
 
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.SoftReference;
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.WeakHashMap;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class SimpleArticleService implements ArticleService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleArticleService.class.getSimpleName());
@@ -34,44 +24,8 @@ public class SimpleArticleService implements ArticleService {
         for (int i = 0; i < count; i++) {
             LOGGER.info("Сгенерирована статья № {}", i);
             Article article = articleGenerator.generate(words);
-            ReferenceQueue<? extends Article> referenceQueue = new ReferenceQueue();
-            SoftReference<Article> softReference = new SoftReference(article, referenceQueue);
+            articleStore.save(article);
             article = null;
-            Article articleFromReference = softReference.get();
-            if (articleFromReference != null) {
-                articleStore.save(articleFromReference);
-                softReference.clear();
-            } else {
-                articleStore.save((Article) referenceQueue.poll());
-            }
-
-
-            //articleStore.save(article);  не работает  переполнение памяти
-            //article = null;
-
-
-//            не работает  переполнение памяти
-//            SoftReference<Article> softReference = new SoftReference<>(article);
-//            article = null;
-//
-//            Article articleFromReference = softReference.get();
-//            if (articleFromReference != null) {
-//                articleStore.save(articleFromReference);
-//                softReference.clear();
-//            }
-
-
-
-//              не работает  переполнение памяти
-//            WeakReference<Article> weakReference = new WeakReference<>(article);
-//            article = null;
-//
-//            Article articleFromReference = weakReference.get();
-//            if (articleFromReference != null) {
-//                articleStore.save(articleFromReference);
-//                weakReference.clear();
-//            }
-
         }
     }
 }
